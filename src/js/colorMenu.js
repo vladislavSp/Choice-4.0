dcd013fd0b309c7d5a0eb6eb994c5d8a773ef372
Difinition <vladdifinition@mail.ru>
2020-04-29 18:34:14 +0300

    Доработки цвета ссылки
    при переходе на темные блоки и обратно на белые

--- src/js/colorMenu.js
+++ src/js/colorMenu.js
@@ -20,3 +20,36 @@ function changeColorHandler() {
   if (blockTop < headerHeight && positionBlock > 0) header.setAttribute('color', 'change');
   else header.setAttribute('color', '');
 }
+
+
+
+// Цвет GetinTouch
+let link = document.querySelector('[link-color="black"]'),
+    blocks = Array.from(document.querySelectorAll('*[link-color="block"]'));
+
+if (link) document.addEventListener('scroll', colorLinkHandler);
+
+function colorLinkHandler() {
+  let newBlocks = blocks.filter(el => el.getBoundingClientRect().width > 0);
+  let elements = newBlocks.map(el => Math.abs(el.getBoundingClientRect().y + el.getBoundingClientRect().height/2)); // el.getBoundingClientRect().y > link.getBoundingClientRect()
+
+  const indexElement = elements.indexOf(Math.min(...elements));
+
+  let linkTop = link.getBoundingClientRect().top,
+      linkHeight = link.getBoundingClientRect().height/2,
+      blockTop = newBlocks[indexElement].getBoundingClientRect().top,
+      blockBottom = newBlocks[indexElement].getBoundingClientRect().bottom,
+      blockHeight = newBlocks[indexElement].getBoundingClientRect().height;
+
+  if (linkTop > (blockTop - linkHeight) && linkTop < (blockBottom - linkHeight)) link.setAttribute(`link-color`, `white`);
+  else link.setAttribute(`link-color`, `black`);
+}
+
+function getCoords(elem) {
+  let box = elem.getBoundingClientRect();
+
+  return {
+    top: box.top + pageYOffset,
+    left: box.left + pageXOffset
+  };
+}
