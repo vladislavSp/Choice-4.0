000e5f269f76abc88716b500e9730af6f20077b2
Difinition <vladdifinition@mail.ru>
2020-05-14 16:39:33 +0300

    Update dzen.js
    Доработка эффекта - добавление посднего появления с 40%

--- src/js/dzen.js
+++ src/js/dzen.js
@@ -4,22 +4,21 @@ let bodyFlag = document.querySelector('[attr="dzen"]'),
     previewBlocks = Array.from(document.querySelectorAll('[lazy-load="preview"]')),
     viewBlocks = Array.from(document.querySelectorAll('[lazy-load="block"]')),
     header = document.querySelector('[hr="color"]');
-    let wrapperBig = document.querySelector('.wrapper--big');
+    let wrapperBorder = document.querySelector('.flip__text');
 
-let index = 0;
-const hundredPercent = 100, timerTime = 500; // initScale = 0.85  valueScale = 0.0015
-let initScale = wrapperBig.getBoundingClientRect().width/window.innerWidth;
+if (bodyFlag) {
+  let index = 0;
+  const hundredPercent = 100, beginScale = 40, timerTime = 500;
+  let initScale = wrapperBorder.getBoundingClientRect().width/window.innerWidth;
+  if (window.matchMedia("(max-width: 479px)").matches) initScale = 0.87; // фикс скейла на мобильных
 
-if (window.matchMedia("(max-width: 479px)").matches) initScale = 0.85; // фикс скейла на мобильных
+  let valueScale = (1 - initScale) / hundredPercent;
 
-let valueScale = (1 - initScale)/hundredPercent;
+  previewBlocks.forEach(el => el.style.transform = `scale(${initScale})`);
+  previewBlocks.forEach(el => el.style.transform = `matrix( ${initScale}, 0, 0, ${initScale}, 0, 0)`);
 
-
-previewBlocks.map(el => el.style.transform = `scale(${initScale})`);
-
-previewBlocks.map(el => el.style.transform = `matrix( ${initScale}, 0, 0, ${initScale}, 0, 0)`);
-
-if (bodyFlag) document.addEventListener('scroll', controllBlockHandler);
+  document.addEventListener('scroll', controllBlockHandler);
+}
 
 function controllBlockHandler(evt) {
 // window.pageYOffset; // текущая прокрутка документа
@@ -30,18 +29,18 @@ function controllBlockHandler(evt) {
 
   let positionFlag = Math.floor(hundredPercent - ((checkBlocks[index].getBoundingClientRect().height + checkBlocks[index].getBoundingClientRect().top) / window.innerHeight) * hundredPercent);
 
-  console.log(positionFlag, valueScale); // позиция блока относительно вьюпорта
+  // console.log(positionFlag); // позиция блока относительно вьюпорта
 
 // Если скролл зашел за половину блока (время для подзагрузки)
   if ((window.innerHeight - checkBlocks[index].getBoundingClientRect().top) > checkBlocks[index].clientHeight / 2) {
 
     viewBlocks[index].setAttribute('state', 'enable'); // показ следующего скрытого блока
+    let positionRatio = (positionFlag - beginScale) * hundredPercent / (hundredPercent - beginScale); // соотношение для позиции % scale = beginScale
 
-    if (positionFlag < 40) {
+    if (positionFlag < beginScale) {
       previewBlocks[index].style.transform = `matrix( ${initScale}, 0, 0, ${initScale}, 0, 0)`; // scale(0.0${positionFlag})
     } else if (positionFlag < hundredPercent) {
-      console.log(valueScale * positionFlag);
-      previewBlocks[index].style.transform = `matrix( ${initScale + valueScale * positionFlag - 0.11875}, 0, 0, ${initScale + valueScale * positionFlag - 0.11875}, 0, 0)`; // матрица преобразования
+      previewBlocks[index].style.transform = `matrix( ${initScale + (valueScale * positionRatio)}, 0, 0, ${initScale + (valueScale * positionRatio)}, 0, 0)`; // матрица преобразования
     }
     // else if (positionFlag >= 10 && positionFlag < hundredPercent) {
     //   previewBlocks[index].style.transform = `scale(0.9${positionFlag})`;
