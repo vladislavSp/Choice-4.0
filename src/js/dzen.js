57cd7cf6078df165de15493b0514abb22db5112f
Difinition <vladdifinition@mail.ru>
2020-05-14 19:35:36 +0300

    Update dzen.js

--- src/js/dzen.js
+++ src/js/dzen.js
@@ -5,10 +5,10 @@ let bodyFlag = document.querySelector('[attr="dzen"]'),
     viewBlocks = Array.from(document.querySelectorAll('[lazy-load="block"]')),
     header = document.querySelector('[hr="color"]');
     let wrapperBorder = document.querySelector('.flip__text');
+    const hundredPercent = 100, beginScale = 40, timerTime = 500;
+    let index = 0;
 
 if (bodyFlag) {
-  let index = 0;
-  const hundredPercent = 100, beginScale = 40, timerTime = 500;
   let initScale = wrapperBorder.getBoundingClientRect().width/window.innerWidth;
   if (window.matchMedia("(max-width: 479px)").matches) initScale = 0.87; // фикс скейла на мобильных
 
@@ -18,64 +18,63 @@ if (bodyFlag) {
   previewBlocks.forEach(el => el.style.transform = `matrix( ${initScale}, 0, 0, ${initScale}, 0, 0)`);
 
   document.addEventListener('scroll', controllBlockHandler);
-}
 
-function controllBlockHandler(evt) {
-// window.pageYOffset; // текущая прокрутка документа
-// checkBlocks[0].clientHeight; // высота блока
-// window.innerHeight; // видимая область экрана viewport
-// window.innerHeight - checkBlocks[0].getBoundingClientRect().top; // состояние, когда вычисляется расстояние до блока-флага
-// checkBlocks[0].getBoundingClientRect().top // высота блока-флага до
+  function controllBlockHandler(evt) {
+  // window.pageYOffset; // текущая прокрутка документа
+  // checkBlocks[0].clientHeight; // высота блока
+  // window.innerHeight; // видимая область экрана viewport
+  // window.innerHeight - checkBlocks[0].getBoundingClientRect().top; // состояние, когда вычисляется расстояние до блока-флага
+  // checkBlocks[0].getBoundingClientRect().top // высота блока-флага до
 
-  let positionFlag = Math.floor(hundredPercent - ((checkBlocks[index].getBoundingClientRect().height + checkBlocks[index].getBoundingClientRect().top) / window.innerHeight) * hundredPercent);
+    let positionFlag = Math.floor(hundredPercent - ((checkBlocks[index].getBoundingClientRect().height + checkBlocks[index].getBoundingClientRect().top) / window.innerHeight) * hundredPercent);
 
-  // console.log(positionFlag); // позиция блока относительно вьюпорта
+    // console.log(positionFlag); // позиция блока относительно вьюпорта
 
-// Если скролл зашел за половину блока (время для подзагрузки)
-  if ((window.innerHeight - checkBlocks[index].getBoundingClientRect().top) > checkBlocks[index].clientHeight / 2) {
+  // Если скролл зашел за половину блока (время для подзагрузки)
+    if ((window.innerHeight - checkBlocks[index].getBoundingClientRect().top) > checkBlocks[index].clientHeight / 2) {
 
-    viewBlocks[index].setAttribute('state', 'enable'); // показ следующего скрытого блока
-    let positionRatio = (positionFlag - beginScale) * hundredPercent / (hundredPercent - beginScale); // соотношение для позиции % scale = beginScale
+      viewBlocks[index].setAttribute('state', 'enable'); // показ следующего скрытого блока
+      let positionRatio = (positionFlag - beginScale) * hundredPercent / (hundredPercent - beginScale); // соотношение для позиции % scale = beginScale
 
-    if (positionFlag < beginScale) {
-      previewBlocks[index].style.transform = `matrix( ${initScale}, 0, 0, ${initScale}, 0, 0)`; // scale(0.0${positionFlag})
-    } else if (positionFlag < hundredPercent) {
-      previewBlocks[index].style.transform = `matrix( ${initScale + (valueScale * positionRatio)}, 0, 0, ${initScale + (valueScale * positionRatio)}, 0, 0)`; // матрица преобразования
-    }
-    // else if (positionFlag >= 10 && positionFlag < hundredPercent) {
-    //   previewBlocks[index].style.transform = `scale(0.9${positionFlag})`;
-    // }
-    else if (positionFlag > hundredPercent ) { // если scroll достиг верx-части экрана
-      previewBlocks[index].classList.add('project-effect--next');
-      previewBlocks[index].style.transform = `matrix( 1, 0, 0, 1, 0, 0)`;
-    // preloadNextProjects(); загрузка след.шаблона
+      if (positionFlag < beginScale) {
+        previewBlocks[index].style.transform = `matrix( ${initScale}, 0, 0, ${initScale}, 0, 0)`; // scale(0.0${positionFlag})
+      } else if (positionFlag < hundredPercent) {
+        previewBlocks[index].style.transform = `matrix( ${initScale + (valueScale * positionRatio)}, 0, 0, ${initScale + (valueScale * positionRatio)}, 0, 0)`; // матрица преобразования
+      }
+      // else if (positionFlag >= 10 && positionFlag < hundredPercent) {
+      //   previewBlocks[index].style.transform = `scale(0.9${positionFlag})`;
+      // }
+      else if (positionFlag > hundredPercent ) { // если scroll достиг верx-части экрана
+        previewBlocks[index].classList.add('project-effect--next');
+        previewBlocks[index].style.transform = `matrix( 1, 0, 0, 1, 0, 0)`;
+      // preloadNextProjects(); загрузка след.шаблона
 
-    //  document.querySelector('body').style.overflow = 'hidden'; // position top
-      scrollControl('lock');
+      //  document.querySelector('body').style.overflow = 'hidden'; // position top
+        scrollControl('lock');
 
-      projectSections[index].remove(); // ;style.display = 'none'
+        projectSections[index].remove(); // ;style.display = 'none'
 
-      scrollControl('unlock');
-    //  document.querySelector('body').style.overflowY = 'scroll';
+        scrollControl('unlock');
+      //  document.querySelector('body').style.overflowY = 'scroll';
 
-      index++;
-      setTimeout(timerOverflowed, timerTime);
-
-      header.classList.remove('header--hid');
-
-      if (index >= checkBlocks.length - 1) {
-        document.removeEventListener('scroll', controllBlockHandler);
+        index++;
         setTimeout(timerOverflowed, timerTime);
+
+        header.classList.remove('header--hid');
+
+        if (index >= checkBlocks.length - 1) {
+          document.removeEventListener('scroll', controllBlockHandler);
+          setTimeout(timerOverflowed, timerTime);
+        }
       }
     }
   }
+
+  function timerOverflowed() {
+    document.querySelector('body').style.overflow = '';
+  }
 }
 
-function timerOverflowed() {
-  document.querySelector('body').style.overflow = '';
-}
-
-//
 // function preloadNextProjects() {
 //   viewBlocks[index].setAttribute('state', 'enable');
 //  viewNextBlock();
