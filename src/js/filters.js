28fbda49a1b85efcce7a448f4150bcc4de504701
Владислав <41942201+vladislavSp@users.noreply.github.com>
2020-07-02 15:24:34 +0300

    Обновление сетки

--- src/js/filters.js
+++ src/js/filters.js
@@ -4,6 +4,10 @@ let filters = [...document.querySelectorAll('*[data-filter]')], filter,
     projectItems = [...document.querySelectorAll('[data-category]')];
 // let update = false;
 
+let initialProjectArray = projectItems.map(el => el.setAttribute('data-sort', 0));
+
+console.log(initialProjectArray);
+
 // ПОКАЗ БЛОКОВ
 if (viewBtn) {
   viewBtn.addEventListener('click', viewBlockHandler);
@@ -14,10 +18,10 @@ if (viewBtn) {
 if (filters.length > 0) {
   filters.forEach(el => el.addEventListener('click', changeStateFilters));
 
-  filter = location.search.split('=')[1];
+  filter = location.search.split('=')[1]; // находим фильтр в строке адреса
 
   if (filter) {
-    setFilter(filter); //, update
+    setFilter(filter); // update
     filters.forEach(el => {
       if (el.getAttribute('data-filter') === filter) el.setAttribute('data-state', 'enable');
       else el.setAttribute('data-state', 'disable');
@@ -40,8 +44,12 @@ function changeStateFilters(evt) {
   if (clickFilter === 'all') {
     gsap.to(projectList, { opacity: 0, onComplete: () => {
       projectItems.forEach( el => el.setAttribute('data-visible', 'true') );
+
+      sortProject(initialProjectArray);
+
       gsap.to(projectList, { opacity: 1, duration: 0.5});
     }});
+
     window.history.replaceState(clickFilter, 'data-filter', `${location.pathname}`);
     stateVisibleProject(false);
   } else {
@@ -53,13 +61,48 @@ function changeStateFilters(evt) {
 function setFilter(clickFilter, upd = true) {
   let visibleItems = projectItems.filter(item => item.getAttribute('data-category').split('||').includes(clickFilter)); // Элементы, которые надо показать (другие скрыть)
 
+  // Добавление порядка для сортировки
+  projectItems.forEach(el => el.setAttribute('data-sort', 2));
+
+  visibleItems.forEach(el => {
+    if (el.getAttribute('data-flagman') === clickFilter) el.setAttribute('data-sort', 0);
+    else el.setAttribute('data-sort', 1);
+  });
+
   gsap.to(projectList, { opacity: 0, duration: 0.5, onComplete: () => {
     projectItems.forEach(el => el.setAttribute('data-visible', 'false'));
     visibleItems.forEach(el => el.setAttribute('data-visible', 'true'));
+
+    sortProject(projectItems);
+
     gsap.to(projectList, { opacity: 1, duration: 0.5});
   }});
 
   stateVisibleProject(true);
+}
+
+
+
+// Функция сортировки дом-элементов
+function sortProject(items, filter) {
+  let itemsArray = [];
+  let parent = items[0].parentNode;
+
+  for (let i = 0; i < items.length; i++) {
+    itemsArray.push(parent.removeChild(items[i]));
+  }
+
+  itemsArray.sort(function(a, b) {
+    let numA = Number(a.getAttribute('data-sort'));
+    let numB = Number(b.getAttribute('data-sort'));
+
+    if (numA < numB) return -1;
+    if (numA > numB) return 1;
+    return 0;
+  })
+  .forEach(function(node) {
+    parent.appendChild(node)
+  });
 }
 
 function stateVisibleProject(f) {
