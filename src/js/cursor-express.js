bdc7e42b0cefce535c27263bc69c2a60dc7a19e9
Владислав <41942201+vladislavSp@users.noreply.github.com>
2020-07-14 18:55:58 +0300

    Update cursor-express.js

--- src/js/cursor-express.js
+++ src/js/cursor-express.js
@@ -4,20 +4,27 @@ if (expressBlock) {
   let imageCursor = document.querySelector('[data-image="express-arrow"]');
   expressBlock.addEventListener('mousemove', cursorChange);
 
+  console.log(imageCursor);
+
   function cursorChange(evt) {
     let target = evt.target;
+    imageCursor.style.display = '';
 
-    imageCursor.style.top = evt.offsetY;
-    imageCursor.style.left = evt.offsetX;
-  
-    if (target.classList.contains('view__image-right')) console.log(`RIGHT`);
-    if (target.classList.contains('view__image-left')) console.log(`LEFT`);
+    imageCursor.style.top = `${evt.offsetY}px`;
+    imageCursor.style.left = `${evt.offsetX}px`;
+
+    // imageCursor.style.top = `${evt.offsetY}px`;
+    // imageCursor.style.left = `${evt.offsetX}px`;
+
+    // if (target.classList.contains('view__image-right')) console.log(`RIGHT`);
+    // if (target.classList.contains('view__image-left')) console.log(`LEFT`);
 
     expressBlock.addEventListener('mouseleave', cursorReset);
   }
 
   function cursorReset() {
     console.log('RESET');
+    imageCursor.style.display = 'none';
   }
 }
 
