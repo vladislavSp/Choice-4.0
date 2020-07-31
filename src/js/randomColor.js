0cea2d4440dd3e2f15e56183388448ba9a57da7f
Владислав <41942201+vladislavSp@users.noreply.github.com>
2020-07-31 14:44:38 +0300

    Update
    - Обновляет цвет у плеера
    - Обновляет в форме пины
    - Добавляет JSON для поиска img

--- src/js/randomColor.js
+++ src/js/randomColor.js
@@ -1,9 +1,6 @@
 const colorArr = [`#F2C94C`, `#27AE60`, `#EB5757`, `#2F80ED`];
 const btn = document.querySelector(`.contact__link`);
 const hoverCompany = document.querySelector('.clients__your-company');
-const videoControls = document.querySelector('.plyr__progress input[type=range]');
-const volumeVideo = document.querySelector('.plyr__volume input[type=range]');
-
 
 createElement();
 randomColorGenerate();
@@ -26,13 +23,8 @@ function randomColorGenerate() {
   localStorage.setItem('randomColor', randColor);
   if (btn) btn.style.backgroundColor = `${randColor}`;
   if (hoverCompany) hoverCompany.style.color = `${randColor}`;
-//  document.body.style.selection = `color: ${randColor}`;
-  if (videoControls || volumeVideo) {
-    videoControls.style.color = randColor;
-    volumeVideo.style.color = randColor;
-  }
 
-  const style = `<style>*::user-select{backround-color:${randColor}}*::selection {background-color: ${randColor}}*::-moz-selection{background-color: ${randColor}} .plyr--video .plyr__control:hover{background:${randColor}}.plyr__control--overlaid{background:${randColor}}</style>`;
+  const style = `<style>*::user-select{backround-color:${randColor}}*::selection {background-color: ${randColor}}*::-moz-selection{background-color: ${randColor}}</style>`;
   divElem.innerHTML = `${style}`;
 }
 
