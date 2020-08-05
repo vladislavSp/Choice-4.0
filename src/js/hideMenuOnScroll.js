082772927c80f1bacbd0ef61c35ae48d2fa00bd0
Владислав <41942201+vladislavSp@users.noreply.github.com>
2020-08-05 18:10:24 +0300

    Update
    - Обновление стилей для картинок
    - Рефакторинг

--- src/js/hideMenuOnScroll.js
+++ src/js/hideMenuOnScroll.js
@@ -1,19 +1,19 @@
-let header = document.querySelector('[data-hr="scroll"]'),
+let headerScroll = document.querySelector('[data-hr="scroll"]'),
     positionInit = pageYOffset; // начальная прокрутка док-та
 
 document.addEventListener('scroll', hideMenuHandler);
 
 function hideMenuHandler(evt) {
-  let heightHeader = header.getBoundingClientRect().height; // 121
+  let heightHeader = headerScroll.getBoundingClientRect().height; // 121
   let dif = pageYOffset - positionInit; // разница текущей прокрутки с изначальной
 
   if (pageYOffset < heightHeader/2) dif = -1; // вначале страницы
 
   if (dif > 30) {
     positionInit = pageYOffset; // увел-е начальной позиции в соответствии с прокруткой
-    header.classList.add('header--hid');
+    headerScroll.classList.add('header--hid');
   } else if (dif < 0) {
     positionInit = pageYOffset;
-    header.classList.remove('header--hid');
+    headerScroll.classList.remove('header--hid');
   }
 }
